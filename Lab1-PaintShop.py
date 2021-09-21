4caa792b697c66c90965c18cdd93c0a7693d483f
Samuel Cowie <w0456204@nscc.ca>
2021-09-21 20:54:19 -0300

    finished all the program

--- Lab1-PaintShop.py
+++ Lab1-PaintShop.py
@@ -13,12 +13,21 @@
 
 def main():
     # YOUR CODE STARTS HERE, each line must be indented (one tab)
-    print("hey there")
+        print("This Program Will Calculate How Much Gallons Of Paint Needed To Paint The Room")
 
+    #this is where i declare my variables
+        firstWidth = input("What is the width of the longer parallel set of walls in square feet?:")
+        secondWidth = input("What is the width of the shorter parallel set of walls in square feet?:")
+        roomHeight = input("What is the height of the room in square feet?:")
+        multiple = int(2) #This number 2 is for the program to calculate the formula for the perimeter 2(p+l)
+        sqaureFeet = int(150) #declaring the 150 sqaurefeet as an int
 
+    #This is where my calculations are done
+        perimeter = (int(firstWidth) + int(secondWidth)) * multiple #This line is calculating the perimeter of the rectangle
+        wallArea = perimeter * int(roomHeight) #This line is calculating the size of the wall area
+        oneGallonofPaint = wallArea / sqaureFeet #dividing wall area by the square feet to determine how much gallons of paint i will need
 
-
-
+        print("You will need", oneGallonofPaint, "gallons of paint for your room!")
 
 
     # YOUR CODE ENDS HERE
